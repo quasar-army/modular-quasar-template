5122de0460dd13557aaeed83ef018bf09df005c8
Luke Diebold <luke@system76-pc.localdomain>
2022-10-16 09:51:58 +1000

    restructure

--- quasar.config.js
+++ quasar.config.js
@@ -22,9 +22,7 @@ module.exports = configure(function (/* ctx */) {
     // preFetch: true,
 
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [
-      'i18n',
-    ],
+    boot: [],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: [
