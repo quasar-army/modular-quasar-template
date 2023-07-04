f802da90bb240d913ae91103a9090a3c75dfb937
Luke Diebold <luke@ldiebold.com>
2023-07-04 17:16:53 +1000

    fix page generator

--- _templates/CONSTANTS.js
+++ _templates/CONSTANTS.js
@@ -6,5 +6,6 @@ module.exports = {
     baseComponent: path.resolve(__dirname, '../src/shared/components/base'),
     views: path.resolve(__dirname, '../src/views'),
     components: path.resolve(__dirname, '../src/components'),
+    pages: path.resolve(__dirname, '../src/pages'),
   },
 }
